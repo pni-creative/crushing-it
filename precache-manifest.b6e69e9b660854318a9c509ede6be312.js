54e23e70f266a6f43b563413e494bdb5f2408259
Pablo <pablo@pdiaz.ca>
2019-08-17 11:23:24 -0700

    Updates

---
+++ precache-manifest.b6e69e9b660854318a9c509ede6be312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f03d348612476b8f89693bed4a55a4d",
+    "url": "/crushing-it/index.html"
+  },
+  {
+    "revision": "57c18f4f6a4caea9eacc",
+    "url": "/crushing-it/static/css/main.106bf1b4.chunk.css"
+  },
+  {
+    "revision": "9e88cc6d837d1ca3e923",
+    "url": "/crushing-it/static/js/2.9dc20dbf.chunk.js"
+  },
+  {
+    "revision": "57c18f4f6a4caea9eacc",
+    "url": "/crushing-it/static/js/main.18a3ca07.chunk.js"
+  },
+  {
+    "revision": "11d969ddcba7259856ba",
+    "url": "/crushing-it/static/js/runtime~main.08ed9033.js"
+  }
+]);
